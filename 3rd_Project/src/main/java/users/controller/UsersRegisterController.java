6e6a101672b1f1a6a259489c17241d8bd3021ea7
hyejuPark <phj__0916@naver.com>
2023-12-08 13:53:58 +0900

    hj 13:53

--- 3rd_Project/src/main/java/users/controller/UsersRegisterController.java
+++ 3rd_Project/src/main/java/users/controller/UsersRegisterController.java
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import users.model.UsersBean;
 import users.model.UsersDao;
-
+ 
 @Controller
 public class UsersRegisterController {
 	private final String command = "/register.u";
 	private final String viewPage = "usersRegisterForm";
 	private final String viewPage2 = "usersWelcomeView";
 	@Autowired
+	
 	private UsersDao ud;
 	
 	@RequestMapping(value = command, method = RequestMethod.GET)
