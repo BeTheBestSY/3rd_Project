a9072f8dd0c664b4e11b75d6429743b9daa4930e
SY <h6118@naver.com>
2023-12-13 17:13:24 +0900

    sy 17:13

--- 3rd_Project/src/main/java/users/controller/UsersKakaoController.java
+++ 3rd_Project/src/main/java/users/controller/UsersKakaoController.java
@@ -1,25 +1,21 @@
 package users.controller;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import users.model.UsersBean;
 import users.model.UsersDao;
-import utility.NaverUsers;
+
 
 @Controller
 public class UsersKakaoController {
