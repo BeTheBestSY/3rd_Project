d092e6d1261e6c03807682dd94dee3d68fc923b2
maneulda <hdaoni1123@gmail.com>
2023-12-17 16:58:13 +0900

    do

--- 3rd_Project/src/main/java/admin/contoller/AdminCelebController.java
+++ 3rd_Project/src/main/java/admin/contoller/AdminCelebController.java
@@ -8,23 +8,27 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import admin.model.AdminDao;
 import celeb.model.CelebBean;
+import celeb.model.CelebDao;
 import utility.Paging;
 
+@Controller
 public class AdminCelebController {
-	private final String command = "/celebList.admin";
+	
+	private final String celebCommand = "/celebList.admin";
 	private final String viewPage = "adminCeleb";
 	
 	@Autowired
 	private AdminDao adminDao;
 	
-	@RequestMapping(value=command)
-	public String list(Model model,
+	@RequestMapping(value=celebCommand)
+	public String celebList(Model model,
 					HttpServletRequest request,
 					@RequestParam(required = false) String whatColumn,
 					@RequestParam(required = false) String keyword,
@@ -35,10 +39,10 @@ public class AdminCelebController {
 		map.put("whatColumn", whatColumn);
 		map.put("keyword", "%"+keyword+"%");
 		System.out.println(whatColumn+","+keyword);
-		String ps="6";
+		String ps="1000";
 		 
 		int totalCount = adminDao.getTotalCountCeleb(map);
-		String url = request.getContextPath()+command;
+		String url = request.getContextPath()+celebCommand;
 		Paging pageInfo = new Paging(pageNumber, ps, totalCount, url, whatColumn, keyword);
 		
 		List<CelebBean> celebLists = adminDao.getAllCelebList(map, pageInfo);
@@ -46,4 +50,5 @@ public class AdminCelebController {
 		model.addAttribute("pageInfo", pageInfo); 
 		return viewPage;
 	}
+	
 }
