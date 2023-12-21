52939959574331b0c405f9d6152acf2137d15ca4
maneulda <hdaoni1123@gmail.com>
2023-12-21 22:12:04 +0900

    do

--- 3rd_Project/src/main/java/admin/contoller/AdminCBoardController.java
+++ 3rd_Project/src/main/java/admin/contoller/AdminCBoardController.java
@@ -25,13 +25,8 @@ import utility.Paging;
 
 @Controller 
 public class AdminCBoardController {
-<<<<<<< HEAD
-
-	@Autowired 
-=======
 	
 	@Autowired
->>>>>>> branch 'daoni' of https://github.com/BeTheBestSY/3rd_Project.git
 	private AdminDao adminDao; 
 
 	public final String listCommand="/cBoardList.admin";
