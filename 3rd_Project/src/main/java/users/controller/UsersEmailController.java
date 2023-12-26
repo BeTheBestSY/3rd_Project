38fb30330557b777b2127588d9ee948faa94d19b
BeTheBestSY <h6118@naver.com>
2023-12-26 17:46:40 +0900

    hj 17:46

--- 3rd_Project/src/main/java/users/controller/UsersEmailController.java
+++ 3rd_Project/src/main/java/users/controller/UsersEmailController.java
@@ -2,7 +2,6 @@ package users.controller;
 
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
