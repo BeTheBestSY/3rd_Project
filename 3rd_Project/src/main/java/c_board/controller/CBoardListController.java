cf42d56fd5e088320bf5cd73037bea2f0bb2b68c
BeTheBestSY <h6118@naver.com>
2023-12-27 11:04:51 +0900

    hj 11:04

--- 3rd_Project/src/main/java/c_board/controller/CBoardListController.java
+++ 3rd_Project/src/main/java/c_board/controller/CBoardListController.java
@@ -46,7 +46,7 @@ public class CBoardListController {
 //		for(CBoardBean cb:list) {
 //			System.out.println("CBoardBean의 c_regdate: " + formatter.format(cb.getC_regdate()));
 //			// registerdate를 포맷팅에서 다시 세팅
-//		}
+//		} 
 		// 현재 날짜 구하기
 //		Date now = new Date();
 		//System.out.println(formatter.format(now));
